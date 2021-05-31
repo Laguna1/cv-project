22ddd0d39780a09b70e29824a565e08136f1d06f
Laguna1 <lagunazoomarket@gmail.com>
2021-06-01 01:28:10 +0300

    Hooks(bg-color)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Room() {
+  const [isLit, setLit] = useState(true);
+  
+  return (
+    <div className={`room ${isLit ? "lit" : "dark"}`}>
+    <br />
+  <button onClick={() => setLit(!isLit)}>
+    flip
+  </button>
+  
+  </div>
+  );
+}
+
+ReactDOM.render(<Room />, document.getElementById('root'));
